850972c4a4471925a76eaf511224d30ee03976a7
Tushar Pawar <109202889+ddshsdf@users.noreply.github.com>
2023-09-27 13:26:40 +0530

    Add files via upload

--- connect.php
+++ connect.php
@@ -8,25 +8,12 @@ $database = "userdb";
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 if(!$conn){
-    die("you not connected to the database");
+    echo "Database successfully connected";
 }
-
 else{
-   
-    $username= $_POST['username'];
-    $password= $_POST['password'];
-    
-
-    $sql = "INSERT INTO `users` (`username`, `password`) VALUES ( '$username', '$password')";
-    $result = mysqli_query($conn, $sql);
-
-    if($result){
-        echo "Data Inserted Successfully";
-    }
-    else{
-        echo "Data is Not Inserted";
-    }
-
+    echo "Error";
 }
 
 ?>
+
+
